49febf014094050e621a432d16f48ff8db0b11b4
Evgeni Glushko <jenya1996@gmail.com>
2025-05-11 17:00:45 +0300

    working google api with map inside the app

--- app/src/main/java/com/EvgeniG_EladO_HalelF/myapplication/MainActivity.java
+++ app/src/main/java/com/EvgeniG_EladO_HalelF/myapplication/MainActivity.java
@@ -1,46 +1,19 @@
 package com.EvgeniG_EladO_HalelF.myapplication;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import android.content.Intent;
-
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.libraries.navigation.NavigationActivity;
-import com.google.android.libraries.navigation.NavigationActivityIntentBuilder;
-
 
 public class MainActivity extends AppCompatActivity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        findViewById(R.id.startNavBtn).setOnClickListener(v -> {
-            LatLng origin = new LatLng(31.7683, 35.2137);
-            LatLng destination = new LatLng(32.0853, 34.7818);
-
-            Intent navIntent = new NavigationActivityIntentBuilder()
-                    .setOrigin(origin)
-                    .setDestination(destination)
-                    .build(this);
-
-            startActivity(navIntent);
-        });
-
-
+        // Hardcoded TextView (no XML layout)
+        TextView textView = new TextView(this);
+        textView.setText("MainActivity loaded successfully");
+        textView.setTextSize(24);
+        textView.setPadding(40, 300, 40, 40);
+        setContentView(textView);
     }
 }
---
+++ app/src/main/java/com/EvgeniG_EladO_HalelF/myapplication/NavigationActivity.java
@@ -0,0 +1,41 @@
+package com.EvgeniG_EladO_HalelF.myapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class NavigationActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.map);
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Focus on Tel Aviv
+        LatLng telAviv = new LatLng(32.0853, 34.7818);
+        mMap.addMarker(new MarkerOptions().position(telAviv).title("Tel Aviv"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(telAviv, 14f));
+    }
+}
